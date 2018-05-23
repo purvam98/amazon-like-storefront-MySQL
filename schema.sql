74d168baaff46fded09b606ab7b9315f3fb344e7
purvam98 <35788384+purvam98@users.noreply.github.com>
2018-05-22 21:42:31 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE bamazon_db;
 USE bamazon_db;;
 
 CREATE TABLE products (
-    item_id VARCHAR(30) AUTO_INCREMENT NOT NULL,
+    item_id VARCHAR(30) NOT NULL,
     product_name VARCHAR(300) NOT NULL,
     department_name VARCHAR(100),
     price DECIMAL(10,2),
